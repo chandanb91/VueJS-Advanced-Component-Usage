81e9d123aae4ef00d9d0762856bed2c00d53f871
Chandan <chandan7791@gmail.com>
2018-09-22 14:22:56 +0530

    Default slots and slot defaults

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -2,9 +2,10 @@
     <div>
         <div class="title">
             <slot name="title"></slot>
+            <span style="color: #ccc"><slot name="subtitle">The Subtitle</slot></span>
         </div>
         <div>
-            <slot name="content"></slot>
+            <slot></slot>
         </div>
     </div>
 </template>
